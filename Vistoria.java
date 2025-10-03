178b7e03688a5227df63e7747e92ee2ef2a9a405
IVO.NASCIMENTO <ivo.oliveira.n@gmail.com>
2025-10-03 12:32:51 -0300

    Add files via upload

---
+++ Vistoria.java
@@ -0,0 +1,109 @@
+package model;
+
+import java.sql.Date;
+
+public class Vistoria {
+	private int idVistoria;
+	private Date dataVistoria;
+	private String itensVerificados;
+	private String resultado;
+	private String statusPagamento;
+	private String observacoes;
+	private int idAgendamento;
+	private int idFuncionario;
+	
+public Vistoria() {
+}
+
+//Construtor
+
+public Vistoria(int idVistoria, Date dataVistoria, String itensVerificados, String resultado, String statusPagamento, 
+		String observacoes, int idAgendamento, int idFuncionario) {
+	
+	this.idVistoria = idVistoria;
+	this.dataVistoria = dataVistoria;
+	this.itensVerificados = itensVerificados;
+	this.resultado = resultado;
+	this.statusPagamento = statusPagamento;
+	this.observacoes = observacoes;
+	this.idAgendamento = idAgendamento;
+	this.idFuncionario = idFuncionario;	
+}
+
+//Getters
+
+public int getIdVistoria() {
+	return idVistoria;
+}
+
+public Date getDataVistoria() {
+	return dataVistoria;
+}
+
+public String getItensVerificados() {
+	return itensVerificados;
+}
+
+public String getResultado() {
+	return resultado;
+}
+
+public String getStatusPagamento() {
+	return statusPagamento;
+}
+
+public String getObservacoes() {
+	return observacoes;
+}
+
+public int getIdAgendamento() {
+	return idAgendamento;
+}
+
+public int getIdFuncionario() {
+	return idFuncionario;
+}
+
+//Setters
+
+public void setIdVistoria(int idVistoria) {
+	this.idVistoria = idVistoria;
+}
+
+public void setDataVistoria(Date dataVistoria) {
+	this.dataVistoria = dataVistoria;
+}
+
+public void setItensVerificados(String itensVerificados) {
+	this.itensVerificados = itensVerificados;
+}
+
+public void setResultado(String resultado) {
+	this.resultado = resultado;
+}
+
+public void setStatusPagamento(String statusPagamento) {
+	this.statusPagamento = statusPagamento;
+}
+
+public void setObservacoes(String observacoes) {
+	this.observacoes = observacoes;
+}
+
+public void setIdAgendamento(int idAgendamento) {
+	this.idAgendamento = idAgendamento;
+}
+
+public void setiIdFuncionario(int idFuncionario) {
+	this.idFuncionario = idFuncionario;
+}
+
+//Transforma objeto em uma String legível
+
+public String toString() {
+	return String.format(
+			"Vistoria {idVistoria=%d, dataVistoria=%s, itensVerificados=%s, resultado=%s, "
+			+ "statusPagamento=%s, observacoes=%s, idAgendamento=%d, "
+			+ "idFuncionario=%d}");
+	}
+}
